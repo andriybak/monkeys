739c33550e893485b3c4a780453d4144e9b890b2
dysya92 <dysya92@hotmail.com>
2014-12-07 15:53:23 +0100

    Second commit

--- tests.py
+++ tests.py
@@ -10,53 +10,35 @@ from datetime import datetime, timedelta
 class TestCase(unittest.TestCase):
  
    def test_follow_posts(self):
-        # make four users
-        u1 = User(nickname='john', email='john@example.com')
-        u2 = User(nickname='susan', email='susan@example.com')
-        u3 = User(nickname='mary', email='mary@example.com')
-        u4 = User(nickname='david', email='david@example.com')
+        # make 3 monkeys and 1 to test delete
+        u1 = User(nickname='alex', email='alex@a.com')
+        u2 = User(nickname='david', email='david@a.com')
+        u3 = User(nickname='lisa', email='lisa@a.com')
+
         db.session.add(u1)
         db.session.add(u2)
         db.session.add(u3)
-        db.session.add(u4)
-        # make four posts
+
+        # make 3 posts
         utcnow = datetime.utcnow()
-        p1 = Post(body="post from john", author=u1, timestamp=utcnow + timedelta(seconds=1))
-        p2 = Post(body="post from susan", author=u2, timestamp=utcnow + timedelta(seconds=2))
-        p3 = Post(body="post from mary", author=u3, timestamp=utcnow + timedelta(seconds=3))
-        p4 = Post(body="post from david", author=u4, timestamp=utcnow + timedelta(seconds=4))
+        p1 = Post(body="alex first post", author=u1, timestamp=utcnow + timedelta(seconds=1))
+        p2 = Post(body="hello from david", author=u2, timestamp=utcnow + timedelta(seconds=2))
+        p3 = Post(body="Hi!!!:)", author=u3, timestamp=utcnow + timedelta(seconds=3))
         db.session.add(p1)
         db.session.add(p2)
         db.session.add(p3)
-        db.session.add(p4)
         db.session.commit()
-        # setup the followers
-        u1.follow(u1)  # john follows himself
-        u1.follow(u2)  # john follows susan
-        u1.follow(u4)  # john follows david
-        u2.follow(u2)  # susan follows herself
-        u2.follow(u3)  # susan follows mary
-        u3.follow(u3)  # mary follows herself
-        u3.follow(u4)  # mary follows david
-        u4.follow(u4)  # david follows himself
+
+        # friend them
+        u1.friend(u2)  # alex - david
+        u1.friend(u3)  # alex - lisa
+        u2.friend(u3)  # david - lisa        
         db.session.add(u1)
         db.session.add(u2)
         db.session.add(u3)
-        db.session.add(u4)
         db.session.commit()
         # check the followed posts of each user
-        f1 = u1.followed_posts().all()
-        f2 = u2.followed_posts().all()
-        f3 = u3.followed_posts().all()
-        f4 = u4.followed_posts().all()
-        assert len(f1) == 3
-        assert len(f2) == 2
-        assert len(f3) == 2
-        assert len(f4) == 1
-        assert f1 == [p4, p2, p1]
-        assert f2 == [p3, p2]
-        assert f3 == [p4, p3]
-        assert f4 == [p4]
+        db.session.rollback()
 
 if __name__ == '__main__':
     unittest.main()
